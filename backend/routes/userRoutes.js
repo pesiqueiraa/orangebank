55ad6005a174cfa807af2b9bf2826f745cc410b2
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-11 00:42:11 -0300

    feat: corrige rota para criar usuário e organiza rotas de login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,4 +3,15 @@ const UserController = require("../controllers/UserController");
 const router = express.Router();
 
 // Rota para criar um novo usuário
-router.post("/users", UserController.createUser);
+router.post("/", UserController.createUser);
+
+// Rota para login genérico (email ou CPF)
+router.post("/login", UserController.loginUser);
+
+// Rota para login com email
+router.post("/login/email", UserController.loginWithEmail);
+
+// Rota para login com CPF
+router.post("/login/cpf", UserController.loginWithCpf);
+
+module.exports = router;

91b0ce0b35c5c8fb2acfbc3d1f3eba645a2a1a83
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-11 11:51:33 -0300

    feat: adiciona rotas para gerenciamento de contas

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const usersRoutes = require('./userRoutes');
+const accountRoutes = require('./accountRoutes');
 
 router.use('/users', usersRoutes);
+router.use('/accounts', accountRoutes);
 
 module.exports = router;

f1539f412e4f7163c8a2f40412d2c59941b64d4f
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-11 00:35:18 -0300

    feat: adiciona configuração de rotas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const routes = require('./routes'); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Usar as rotas com prefixo opcional (ex: /api/users)
+app.use('/api', routes);
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta http://localhost:${port}`);

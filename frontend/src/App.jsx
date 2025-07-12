6e05e8b52d370123ff4df63c5f3308965c734b8b
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-11 23:52:23 -0300

    feat: add rota para pagina de registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/index';
 import Login from './pages/Login';
+import Register from './pages/Register';  
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
